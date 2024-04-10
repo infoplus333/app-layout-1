e831f764b6fdd05e8c17c30b9aae1725f0e23cc1
IPMDM63\Haris <mohamedharis.a@infoplusmdm.com>
2024-04-10 17:43:58 +0530

    added tailwind css theme

--- src/app/modules/dictionary/dictionary.component.ts
+++ src/app/modules/dictionary/dictionary.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule } from '@angular/forms';
+
+interface City {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-dictionary',
   standalone: true,
-  imports: [ButtonModule, TableModule],
+  imports: [ButtonModule, TableModule, MultiSelectModule, FormsModule],
   templateUrl: './dictionary.component.html',
   styleUrl: './dictionary.component.scss'
 })
 export class DictionaryComponent {
+  cities!: City[];
 
+  selectedCities!: City[];
+
+  ngOnInit() {
+      this.cities = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
+      ];
+  }
 }
