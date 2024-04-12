100e62d274b97066be7eb580dcba70f423677fa8
Haris418 <mh955490@gmail.com>
2024-04-12 12:21:12 +0530

    seach icon

--- src/app/modules/dictionary/dictionary.component.ts
+++ src/app/modules/dictionary/dictionary.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ChipsModule } from 'primeng/chips';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+
 
 @Component({
   selector: 'app-dictionary',
   standalone: true,
-  imports: [ButtonModule, TableModule],
+  imports: [CheckboxModule, ButtonModule, TableModule, IconFieldModule, InputIconModule, AngularSvgIconModule, ChipsModule, FormsModule, DropdownModule],
   templateUrl: './dictionary.component.html',
   styleUrl: './dictionary.component.scss'
 })
 export class DictionaryComponent {
+  values: string[] | undefined;
+  checked: boolean = false;
 
 }
